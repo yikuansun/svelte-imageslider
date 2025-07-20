1f9755863f1023bd56dab669cd426799a70f441b
Yikuan Sun <yikuansun2015@gmail.com>
2025-07-20 14:56:28 -0400

    Build rollup package
    i dunno how this works :(

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,21 @@
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
+
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
 const pkg = require("./package.json");
+
 export default {
     input: "src/ImageSlider.svelte",
     output: [
         { file: pkg.module, format: "es" },
         { file: pkg.main, format: "umd", name: "Name" },
     ],
-    plugins: [svelte(), resolve()],
+    plugins: [svelte({
+        emitCss: false,
+    }), resolve({
+        browser: true,
+        exportConditions: ["svelte"],
+        extensions: [".mjs", ".js", ".svelte"],
+    })],
 };
