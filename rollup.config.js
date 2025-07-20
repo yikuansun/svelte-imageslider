cb17c6cce277d9f9535b1878153353f3dc5f8882
Yikuan Sun <yikuansun2015@gmail.com>
2025-07-20 14:59:40 -0400

    Use new rollup resolve plugin
    good

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import svelte from "rollup-plugin-svelte";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
